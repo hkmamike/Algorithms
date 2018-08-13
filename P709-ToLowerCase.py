3d9aa005c80c1a4fec8e2288df320b212d9e68be
Mike <mikebrianleung@gmail.com>
2018-08-13 09:22:39 -0700

    added p709

---
+++ P709-ToLowerCase.py
@@ -0,0 +1,17 @@
+class Solution:
+    def toLowerCase(self, str):
+        """
+        :type str: str
+        :rtype: str
+        """
+        caseMap = {'A':'a', 'B':'b','C':'c', 'D':'d', 'E':'e', 'F':'f', 'G':'g', 'H':'h', 'I':'i', 'J':'j', 'K':'k', 'L':'l', 'M':'m', 'N':'n', 'O':'o', 'P':'p', 'Q':'q', 'R':'r', 'S':'s', 'T':'t', 'U':'u', 'V':'v', 'W':'w', 'X':'x', 'Y':'y', 'Z':'z'}
+        
+        result = ''
+        
+        for char in str:
+            if char in caseMap:
+                result += caseMap[char]
+            else:
+                result += char
+                
+        return result
