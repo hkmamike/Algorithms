4067d45ebb0d229f58af1127c5b9536f59b0cf81
Mike Leung <mikebrianleung@gmail.com>
2018-11-02 10:28:18 -0700

    added p377

---
+++ P377-CombinationSumIV.py
@@ -0,0 +1,19 @@
+class Solution:
+    def combinationSum4(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        dp = [0 for i in range(target + 1)]
+        
+        dp[0] = 1
+        
+        for i in range(1, target + 1):
+            for num in nums:
+                if i >= num:
+                    dp[i] += dp[i - num]
+                    
+        return dp[target]
+        
