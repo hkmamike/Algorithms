afaa9c6f6e7616837c209baa4810dd5fdbd5f8e4
Mike Leung <mikebrianleung@gmail.com>
2019-01-13 11:14:30 -0800

    added p976

---
+++ P976-LargestPerimeterTriangle.py
@@ -0,0 +1,15 @@
+class Solution:
+
+
+    def largestPerimeter(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        A = sorted(A, reverse=True)
+        
+        for i in range(0, len(A)-2):
+            if A[i+1] + A[i+2] > A[i]:
+                return A[i] + A[i+1] + A[i+2]
+            
+        return 0
