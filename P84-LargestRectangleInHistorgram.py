6b5fbd823cd453daca6f836199b956319a1145a0
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-04-27 23:42:18 -0700

    Update P84-LargestRectangleInHistorgram.py

--- P84-LargestRectangleInHistorgram.py
+++ P84-LargestRectangleInHistorgram.py
@@ -1,3 +1,20 @@
+
+
+# Referencing clean solutions
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        heights.append(0)
+        stack = [-1]
+        ans = 0
+        for i in range(len(heights)):
+            while heights[i] < heights[stack[-1]]:
+                height = heights[stack.pop()]
+                width = i - stack[-1] - 1
+                ans = max(ans, height * width)
+            stack.append(i)
+        heights.pop()
+        return ans
+
 class Solution:
     def largestRectangleArea(self, heights):
         """
