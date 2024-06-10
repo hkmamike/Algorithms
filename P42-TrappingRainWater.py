530ff4f31d41c6099b0fd57c824a5ab4ed14eb05
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-06-09 22:19:15 -0700

    Update P42-TrappingRainWater.py

--- P42-TrappingRainWater.py
+++ P42-TrappingRainWater.py
@@ -1,3 +1,4 @@
+# easier to understand
 class Solution:
     def trap(self, height):
         """
@@ -21,3 +22,22 @@ class Solution:
             water[i] = max(0, min( left[i], right[i] ) - heights[i])
             
         return sum(water)
+
+# more efficient
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        L, R = 0, len(height) - 1
+        safeLevel = 0
+        totalWater = 0
+
+        while L < R:
+            lower = min(height[L], height[R])
+            safeLevel = max(safeLevel, lower)
+            totalWater += safeLevel - lower
+
+            if height[L] < height[R]:
+                L += 1
+            else:
+                R -= 1
+
+        return totalWater
