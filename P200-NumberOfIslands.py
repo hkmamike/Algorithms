5afa897866ab05e7208818551964e456e01dfbb0
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-07-17 23:21:17 -0700

    Update P200-NumberOfIslands.py

--- P200-NumberOfIslands.py
+++ P200-NumberOfIslands.py
@@ -1,3 +1,36 @@
+
+
+## No extra space
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        H = len(grid)
+        W = len(grid[0])
+
+        def visit(grid, r, c):
+            if grid[r][c] == "0" or grid[r][c] == "-1":
+                return
+            else:
+                grid[r][c] = "-1"
+
+            if r > 0:
+                visit(grid, r-1, c)
+            if r < H - 1:
+                visit(grid, r+1, c)
+            if c > 0:
+                visit(grid, r, c-1)
+            if c < W - 1:
+                visit(grid, r, c+1)
+
+        islandCnt = 0
+        for r in range(H):
+            for c in range(W):
+                if grid[r][c] == "1":
+                    islandCnt += 1
+                    visit(grid, r, c)
+
+        return islandCnt
+        
+
 class Solution:
     def numIslands(self, grid):
         """
