d9396d85f4f0c5f45d7d65c5bc6390d33c1f4ca6
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-07-21 00:46:29 -0700

    P746 second try

--- P746-MinCostClimbingStairs.py
+++ P746-MinCostClimbingStairs.py
@@ -1,4 +1,15 @@
 class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        dp = [0] * (n + 1)
+
+        for i in range(2, n + 1):
+            dp[i] = min(cost[i-1] + dp[i-1], cost[i-2] + dp[i-2])
+
+        return dp[n]
+
+
+class Solution:
     def minCostClimbingStairs(self, cost):
         costSoFar = [0] * (len(cost)+1)
         
