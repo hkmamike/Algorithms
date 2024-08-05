b361d8b5fbd9623ad36dbf43d2f03a123ecd3254
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-08-04 17:34:38 -0700

    Create P146-LRUCache.py

---
+++ P146-LRUCache.py
@@ -0,0 +1,29 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        val = self.cache.pop(key)
+        self.cache[key] = val
+        return val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.pop(key)
+        else:
+            if self.capacity > 0:
+                self.capacity -= 1
+            else:
+                LruKey = iter(self.cache)
+                self.cache.pop(next(LruKey))
+        self.cache[key] = value
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
