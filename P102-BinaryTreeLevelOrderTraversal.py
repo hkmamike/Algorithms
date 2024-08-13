9392610045e0a23227bb258173f22a4b6b651039
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-08-12 22:51:27 -0700

    Update P102-BinaryTreeLevelOrderTraversal.py

--- P102-BinaryTreeLevelOrderTraversal.py
+++ P102-BinaryTreeLevelOrderTraversal.py
@@ -1,5 +1,29 @@
 # Definition for a binary tree node.
 # class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        self.levelsMap = defaultdict(list)
+
+        def dfs(node, level):
+            if not node:
+                return
+            self.levelsMap[level].append(node.val)
+            dfs(node.left, level + 1)
+            dfs(node.right, level + 1)
+
+        dfs(root, 0)
+        result = []
+        for i in range(len(self.levelsMap)):
+            result.append(self.levelsMap[i])
+        return result
+
+
+# Definition for a binary tree node.
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
