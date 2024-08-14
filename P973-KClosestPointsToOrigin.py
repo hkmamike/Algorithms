5a22ae79ca2208eb51c26293867dde16d86139f4
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-08-13 22:55:15 -0700

    Update P973-KClosestPointsToOrigin.py

--- P973-KClosestPointsToOrigin.py
+++ P973-KClosestPointsToOrigin.py
@@ -1,4 +1,20 @@
 class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        
+        def getDistanceFromOrigin(x1, y1):
+            return pow(pow(x1, 2) + pow(y1, 2) , 0.5)
+
+        heap = []
+        for p in points:
+            d = getDistanceFromOrigin(p[0], p[1])
+            if len(heap) < k:
+                heapq.heappush(heap, (-d, p[0], p[1]))
+            else:
+                heapq.heappushpop(heap, (-d, p[0], p[1]))
+
+        return [[h[1], h[2]] for h in heap]
+
+class Solution:
     def kClosest(self, points, K):
         """
         :type points: List[List[int]]
