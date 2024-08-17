1f0bf526af8572114881a4c74a3622c02c91a5db
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-08-16 23:15:53 -0700

    Update P846-HandOfStraights.py

--- P846-HandOfStraights.py
+++ P846-HandOfStraights.py
@@ -6,6 +6,26 @@ class Solution:
             return True
 
         groupCnt = len(hand) // groupSize
+        cardCounter = collections.Counter(hand)
+        
+        for key in sorted(cardCounter.keys()):
+            numCard = cardCounter[key]
+            if numCard > 0:
+                for i in range(key, key+groupSize):
+                    if cardCounter[i] < numCard:
+                        return False
+                    else:
+                        cardCounter[i] -= numCard
+        return True
+
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0:
+            return False
+        if groupSize == 1:
+            return True
+
+        groupCnt = len(hand) // groupSize
         groups = []
 
         hand.sort()
