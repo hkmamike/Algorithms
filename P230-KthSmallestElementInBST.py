e01fdd5066ff3ba67412c75a2d25fb6d89bfa54d
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-08-20 20:17:52 -0700

    Update P230-KthSmallestElementInBST.py

--- P230-KthSmallestElementInBST.py
+++ P230-KthSmallestElementInBST.py
@@ -1,5 +1,28 @@
 # Definition for a binary tree node.
 # class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        
+        self.nodes = []
+        def dfs(node):
+            if node:
+                dfs(node.left)
+                self.nodes.append(node.val)
+                if len(self.nodes) == k:
+                    raise Exception("Solution Found, Terminating")
+                dfs(node.right)
+
+        try:
+            dfs(root)
+        except:
+            return self.nodes[-1]
+
+# Definition for a binary tree node.
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
