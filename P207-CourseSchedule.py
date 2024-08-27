5278dc754d37985613d8b1364437b8be6c94c7f7
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-08-26 22:34:23 -0700

    Update P207-CourseSchedule.py

--- P207-CourseSchedule.py
+++ P207-CourseSchedule.py
@@ -1,3 +1,30 @@
+# New way to do it
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        def doable(node, classesPossible, depMap):
+            if classesPossible[node] == 1:
+                return True
+            if classesPossible[node] == -1:
+                return False
+
+            classesPossible[node] = -1
+            isNodeDoable = all([ doable(prereq, classesPossible, depMap) for prereq in depMap[node] ])
+            if isNodeDoable:
+                classesPossible[node] = 1
+            return isNodeDoable
+            
+        depMap = defaultdict(set)
+        for c, prereq in prerequisites:
+            depMap[c].add(prereq)
+
+        foundationalSet = set(range(numCourses)) - set(depMap.keys())
+        classesPossible = [0] * numCourses
+        for key in foundationalSet:
+            classesPossible[key] = 1
+        
+        return all([doable(c, classesPossible, depMap) for c in range(numCourses)])
+
+# TopologicalSort
 class Solution:
     def canFinish(self, numCourses, prerequisites):
         graph = [[] for _ in range(numCourses)]
