1f1e778be7f36175630d31222c4ddfe2bc6a7720
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-08-28 22:23:35 -0700

    Update P54-SpiralMatrix.py

--- P54-SpiralMatrix.py
+++ P54-SpiralMatrix.py
@@ -1,4 +1,48 @@
 class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        
+        def isOutofBound(r, c, M, N):
+            if r < 0 or r > M - 1:
+                return True
+            if c < 0 or c > N - 1:
+                return True
+            return False
+
+        def isVisited(r, c, visited):
+            if (r, c) in visited:
+                return True
+            return False
+
+        dirMap = {(0, 1):(1, 0), (1, 0):(0, -1), (0, -1):(-1, 0), (-1, 0):(0, 1)}
+        M = len(matrix)
+        N = len(matrix[0])
+        visited = set()
+        visited.add((0, 0))
+        total = M * N
+        cnt = 1
+        result = [matrix[0][0]]
+        curPos = [0, 0]
+        curDir = [0, 1]
+
+        while cnt < total:
+            nextCandidate = [curPos[0] + curDir[0], curPos[1] + curDir[1]]
+
+            # turn if we hit a wall
+            if isOutofBound(nextCandidate[0], nextCandidate[1], M, N) or isVisited(nextCandidate[0], nextCandidate[1], visited):
+                curDir[0], curDir[1] = dirMap[(curDir[0], curDir[1])]
+                nextCandidate = [curPos[0] + curDir[0], curPos[1] + curDir[1]]
+            
+            result.append(matrix[nextCandidate[0]][nextCandidate[1]])
+            visited.add((nextCandidate[0], nextCandidate[1]))
+            curPos[0] = nextCandidate[0]
+            curPos[1] = nextCandidate[1]
+            cnt += 1
+
+        return result
+
+        
+
+class Solution:
     def spiralOrder(self, matrix):
 
         if not matrix:
