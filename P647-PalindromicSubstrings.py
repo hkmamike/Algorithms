97e013424cda34289841c2be1e71f6d985632eba
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-09-03 23:41:33 -0700

    Update P647-PalindromicSubstrings.py

--- P647-PalindromicSubstrings.py
+++ P647-PalindromicSubstrings.py
@@ -7,13 +7,16 @@ class Solution:
         for i in range(n):
             palindrome[i][i] = True
             ans += 1
+
         for i in range(n - 1):
             if s[i] == s[i + 1]:
                 palindrome[i][i + 1] = True
                 ans += 1
+
         for length in range(3, n + 1):
             for i in range(n - length + 1):
                 if s[i] == s[i + length - 1] and palindrome[i + 1][i + length - 2]:
                     palindrome[i][i + length - 1] = True
                     ans += 1
+
         return ans
