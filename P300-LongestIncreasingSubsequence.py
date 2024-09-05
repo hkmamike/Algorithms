09a4e996302c6b20a4deb5f0e0c7a478368b0480
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-09-04 21:11:12 -0700

    Create P300-LongestIncreasingSubsequence.py

---
+++ P300-LongestIncreasingSubsequence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        bestAt = [1] * len(nums)
+        for i, n in enumerate(nums):
+            if i == 0:
+                continue
+            bestLeft = max([bestAt[j] if nums[j] < n else 0 for j in range(i)])
+            bestAt[i] = bestLeft + 1
+        
+        return max(bestAt)
