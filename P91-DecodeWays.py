7a68a482c015ccc447a77000699c8793817faecd
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-09-06 00:35:08 -0700

    Update P91-DecodeWays.py

--- P91-DecodeWays.py
+++ P91-DecodeWays.py
@@ -1,7 +1,16 @@
 class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp = [0, 1]
+        previous = ""
+        for i, c in enumerate(s):
+            dp.append(dp[-1] * (c > "0") + dp[-2] * (9 < int(previous + c) < 27))
+            previous = c
         
-    def numDecodings(self, s):
+        return dp[-1]
 
+
+class Solution:
+    def numDecodings(self, s):
         previousDigit = ''
         previousWays = 0
         
