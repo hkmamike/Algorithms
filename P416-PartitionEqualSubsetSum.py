02b6c5653230ac31a45c3e15c4dde118e4abafbb
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-09-09 22:05:27 -0500

    Update P416-PartitionEqualSubsetSum.py

--- P416-PartitionEqualSubsetSum.py
+++ P416-PartitionEqualSubsetSum.py
@@ -1,3 +1,24 @@
+# Add tabulation table and implement early break by returning conditionals, pass.
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        
+        def find(taraget, idx, N):
+            if target == 0:
+                return True
+            elif idx >= N or target < 0:
+                return False
+            elif (self.dp[idx][target] != -1):
+                return self.dp[idx][target]
+            self.dp[idx][target] = find(target - nums[idx], idx+1, N) or find(target, idx+1, N)
+            return self.dp[idx][target]
+
+        totalSum, N = sum(nums), len(nums)
+        targetSum = totalSum // 2
+        if totalSum % 2 != 0:
+            return False
+        else:
+            self.dp = [[-1] * (targetSum + 1) for _ in range(N)]
+            return find(targetSum, 0, N)
 
 # General Solution, TLE
 class Solution:
