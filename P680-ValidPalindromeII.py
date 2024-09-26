60bf60f763e2ce667d74a32a4bbb207349e2e077
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-09-26 00:11:33 -0700

    Update P680-ValidPalindromeII.py

--- P680-ValidPalindromeII.py
+++ P680-ValidPalindromeII.py
@@ -1,3 +1,21 @@
+# general solution for budget more than 1
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        if len(s) <= 2:
+            return True
+    
+        def recurse(s, L, R, budget):
+            if L >= R:
+                return True
+            elif s[L] == s[R]:
+                return recurse(s, L+1, R-1, budget)
+            elif budget == 0:
+                return False
+            else:
+                return any([recurse(s, L, R-1, budget-1), recurse(s, L+1, R, budget-1)])
+        return recurse(s, 0, len(s)-1, 1)
+
+# optimal solution
 class Solution:
     def validPalindrome(self, s):
         """
