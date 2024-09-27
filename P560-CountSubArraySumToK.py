f9c0bc5be3174d013476d9c7552223ad71f3adc0
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-09-26 20:27:50 -0700

    Update P560-CountSubArraySumToK.py

--- P560-CountSubArraySumToK.py
+++ P560-CountSubArraySumToK.py
@@ -23,4 +23,20 @@ class Solution:
             else:
                 sumsMap[sumSoFar] = 1
                 
-        return count 
+        return count
+    
+# N^2 TLE simple
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        
+        prefixSum = [0]
+        for n in nums:
+            prefixSum.append(prefixSum[-1] + n)
+        
+        result = 0
+        for i in range(len(nums)):
+            for j in range(i, len(nums)):
+                subArraySum = prefixSum[j+1]-prefixSum[i]
+                if subArraySum == k:
+                    result += 1
+        return result
