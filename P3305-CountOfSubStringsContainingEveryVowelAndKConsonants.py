17fa9ba2030bb62eef489f60f797ef79695a6291
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-09-28 22:39:36 -0700

    Update P3305-CountOfSubStringsContainingEveryVowelAndKConsonants.py

--- P3305-CountOfSubStringsContainingEveryVowelAndKConsonants.py
+++ P3305-CountOfSubStringsContainingEveryVowelAndKConsonants.py
@@ -1,5 +1,51 @@
 
+# O(N)
+class Solution:
+    def countOfSubstrings(self, word: str, k: int) -> int:
+        
+        def atLeast(num):
+            charTypes = "aeiouz"
+            L, R, N = 0, 0, len(word)
+            cnt = defaultdict(int)
+            result = 0
 
+            while R < N:
+                # add word[R] into counter
+                ch = word[R]
+                if ch in "aeiou":
+                    cnt[ch] += 1
+                else:
+                    cnt["z"] += 1
+    
+                # check valid condition
+                isValid = True
+                for c in "aeiou":
+                    if cnt[c] == 0:
+                        isValid = False
+                        break
+                if cnt["z"] < num:
+                    isValid = False
+
+                # Shrink L
+                while isValid:
+                    result += (N - R)
+                    ch = word[L]
+                    if ch in "aeiou":
+                        cnt[ch] -= 1
+                    else:
+                        cnt["z"] -= 1
+
+                    isValid = True
+                    for c in "aeiou":
+                        if cnt[c] == 0:
+                            isValid = False
+                            break
+                    if cnt["z"] < num:
+                        isValid = False
+                    L += 1
+                R += 1
+            return result
+        return atLeast(k) - atLeast(k+1)
 
 # N^2
 class Solution:
