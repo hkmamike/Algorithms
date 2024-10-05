b70ca22e20c02e0ef07005d62e1752691a0e324b
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-10-04 19:50:54 -0700

    Update P199-BinaryTreeRightSideView.py

--- P199-BinaryTreeRightSideView.py
+++ P199-BinaryTreeRightSideView.py
@@ -4,6 +4,27 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# Oct 4, 2024
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+
+        def recurse(node, depth, levelMap):
+            if not node:
+                return
+            if depth not in levelMap:
+                levelMap[depth] = node.val
+            recurse(node.right, depth+1, levelMap)
+            recurse(node.left, depth+1, levelMap)
+
+        levelMap = {}
+        recurse(root, 0, levelMap)
+        result = []
+        for i in range(len(levelMap)):
+            result.append(levelMap[i])
+
+        return result
+
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         self.levelTakenSet = set()
