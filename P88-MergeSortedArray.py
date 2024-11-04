ddc5bb217cc22ed7e8851f0c470899b3141e420d
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-11-03 18:39:42 -0800

    Update P88-MergeSortedArray.py

--- P88-MergeSortedArray.py
+++ P88-MergeSortedArray.py
@@ -1,3 +1,30 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        idx = len(nums1) - 1
+        m -= 1
+        n -= 1
+
+        while idx > -1:
+            if n < 0:
+                nums1[idx] = nums1[m]
+                m -= 1
+                idx -= 1
+            elif m < 0:
+                nums1[idx] = nums2[n]
+                n -= 1
+                idx -= 1
+            elif nums1[m] >= nums2[n]:
+                nums1[idx] = nums1[m]
+                m -= 1
+                idx -= 1
+            else:
+                nums1[idx] = nums2[n]
+                n -= 1
+                idx -= 1
+
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
         """
