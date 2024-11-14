74166a13fe40bc762445bd75e107a4e67e83bbe6
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-11-13 23:21:37 -0800

    Update P29-Divide2Integers.py

--- P29-Divide2Integers.py
+++ P29-Divide2Integers.py
@@ -23,3 +23,24 @@ class Solution:
             result = - result
             
         return min(max(result, -2147483648), 2147483647)
+    
+# Idea, TLE
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        positive = (dividend > 0) == (divisor > 0)
+        dividend = abs(dividend)
+        divisor = abs(divisor)
+
+        if divisor == 1:
+            if not positive:
+                dividend = - dividend
+            return min(max(dividend, -2147483648), 2147483647)
+
+        quotient = 0
+        while dividend >= divisor:
+            dividend -= divisor
+            quotient += 1
+
+        if not positive:
+            quotient = - quotient
+        return min(max(quotient, -2147483648), 2147483647)
